bf752445b439c7d14bd9a4a249a574823acb6a5a
Rodrigo <digonalha@gmail.com>
2019-02-06 21:16:14 -0200

    Create auto_loot.py

---
+++ auto_loot.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+from pynput.mouse import Button, Controller as mouse_controller
+from pynput.keyboard import Key, Controller as keyboard_controller
+import time
+
+mouse = mouse_controller()
+keyboard = keyboard_controller()
+
+positions = [
+    (794, 291),
+    (743, 292),
+    (749, 336),
+    (742, 384),
+    (792, 392),
+    (856, 387),
+    (857, 334),
+    (859, 276),
+    (803, 329)
+]
+
+def press_key_fast():
+
+    original_position = mouse.position
+
+    for i in positions:
+        mouse.position = i
+        time.sleep(0.01)
+
+        with keyboard.pressed(Key.shift):
+            mouse.press(Button.right)
+            mouse.release(Button.right)
+            time.sleep(0.01)
+        
+    mouse.position = original_position
+
+press_key_fast()
